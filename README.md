e8f23281c747d7f49944a019f55d5e29c0369b10
0I0u3uk <svetlakovanton@gmail.com>
2017-05-02 16:08:27 +0700

    initial project

---
+++ README.md
@@ -0,0 +1,1 @@
+Это тестовый проект с примером использования фреймворка [page-factory](https://github.com/sbtqa/page-factory)
---
+++ src/test/java/ru/sbtqa/tag/pagefactoryexample/CucumberTest.java
@@ -0,0 +1,19 @@
+package ru.sbtqa.tag.pagefactoryexample;
+
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+import ru.sbtqa.tag.cucumber.TagCucumber;
+
+/**
+ *
+ * @author sbt-svetlakov-av
+ */
+@RunWith(TagCucumber.class)
+@CucumberOptions(monochrome = true, format = {"pretty"},
+        glue = {"ru.sbtqa.tag.pagefactory.stepdefs"},
+        features = {"src/test/resources/features/"},
+        tags = {"@test"})
+
+public class CucumberTest {
+
+}
