5800edb3ab975c4661c9fbe18b7210243bfbd62d
Сидоченко Виктор Викторович <VVSidochenko.SBT@sberbank.ru>
2018-05-23 13:31:32 +0700

    Update to pf 1.10.0

--- src/test/java/ru/sbtqa/tag/pagefactoryexample/CucumberTest.java
+++ src/test/java/ru/sbtqa/tag/pagefactoryexample/CucumberTest.java
@@ -1,11 +1,11 @@
 package ru.sbtqa.tag.pagefactoryexample;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
-import ru.sbtqa.tag.cucumber.TagCucumber;
 
-@RunWith(TagCucumber.class)
+@RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, format = {"pretty"},
-        glue = {"ru.sbtqa.tag.pagefactory.stepdefs"},
+        glue = {"ru.sbtqa.tag.stepdefs.ru"},
         features = {"src/test/resources/features/"})
 public class CucumberTest {}
